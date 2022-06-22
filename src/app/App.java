bc155bf7e1efbc5350644702856a50220e956c39
Julio Guerrero <juliocaramelo44@gmail.com>
2022-06-22 12:20:07 -0500

    agregado al App el listener para subir los archivos seleccionados, ya lee archivos xlsx

--- src/app/App.java
+++ src/app/App.java
@@ -2,15 +2,34 @@ package app;
 
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Iterator;
 import java.util.List;
 import java.util.ArrayList;
 
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
+
+
+
+
+
+
+
+
+
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;  
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;  
+import org.apache.poi.ss.usermodel.Cell;  
+import org.apache.poi.ss.usermodel.FormulaEvaluator;  
+import org.apache.poi.ss.usermodel.Row;  
 
 import vistas.Index;
 import modelos.Compranet;
@@ -33,12 +52,14 @@ public class App {
 	private Procedimiento procedimiento;
 	private Connection conn = new Conexion().getConnection();
 	private String path = "";
+	private File selected = null;
+	
 
 	public App(/*
 				 * Index index, Compranet compranet, ClaveContrato
 				 * claveContrato, OrdenReposicion ordenReposicion, Procedimiento
 				 * procedimiento
-				 */) throws SQLException {
+				 */) throws SQLException, IOException {
 		this.index = new Index();
 		this.compranet = new Compranet();
 		this.claveContrato = new ClaveContrato();
@@ -46,7 +67,7 @@ public class App {
 		this.procedimiento = new Procedimiento();
 
 		this.index.getBuscarButton().addActionListener(Buscar());
-
+		this.index.getSubirButton().addActionListener(Subir());
 	}
 
 	public void start() {
@@ -60,19 +81,60 @@ public class App {
 	private ActionListener Buscar() throws SQLException {
 		ActionListener Buscar = ewt -> {
 			JFileChooser path = new JFileChooser();
-			path.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-			path.setCurrentDirectory(new File("c:/windows/"));
-			FileNameExtensionFilter filter = new FileNameExtensionFilter("Archivos csv", "csv");
+			path.setFileSelectionMode(JFileChooser.FILES_ONLY);
+			path.setCurrentDirectory(new File("c:/windows/%UserProfile%/Escritorio"));
+			FileNameExtensionFilter filter = new FileNameExtensionFilter("Archivos csv", "xlsx");
 			path.setFileFilter(filter);
 			
 			int respuesta = path.showOpenDialog(path);
 			if(respuesta == JFileChooser.APPROVE_OPTION){
-				File selected = path.getSelectedFile();
-				this.path = selected.getPath();
+				this.selected = path.getSelectedFile();
+				this.path = this.selected.getPath();
 				this.index.getTextPath().setText(this.path);
 			}
 			
 		};
 		return Buscar;
 	}
+	
+	private ActionListener Subir() throws SQLException, IOException{
+		@SuppressWarnings("deprecation")
+		ActionListener Subir = ewt -> {
+			try {
+				FileInputStream files = new FileInputStream (new File(this.path));
+				XSSFWorkbook  workbook = new XSSFWorkbook(files);
+				
+				XSSFSheet sheet = workbook.getSheetAt(0);
+				FormulaEvaluator formulaEvaluator = workbook.getCreationHelper().createFormulaEvaluator();  
+				Iterator<Row> itr = sheet.iterator();    //iterating over excel file  
+				while (itr.hasNext())                 
+				{  
+					Row row = itr.next();  
+					Iterator<Cell> cellIterator = row.cellIterator();   //iterating over each column  
+					while (cellIterator.hasNext()){  
+						Cell cell = cellIterator.next();  
+						switch (cell.getCellType()){  
+						case Cell.CELL_TYPE_STRING:    //field that represents string cell type  
+							System.out.print(cell.getStringCellValue() + "\t\t\t");  
+						break;  
+						case Cell.CELL_TYPE_NUMERIC:    //field that represents number cell type  
+							System.out.print(cell.getNumericCellValue() + "\t\t\t");  
+						break;  
+						default:  
+						}  
+					}  
+					System.out.println("");  
+				}  
+
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}catch(IOException e){
+				e.printStackTrace();
+			}	
+		};
+		return Subir;
+	}
+
 }
+
