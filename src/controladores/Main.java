1e5c45ddb6076f3da57632a1fe229e62fbb193b9
Julio Guerrero <juliocaramelo44@gmail.com>
2022-06-22 12:20:38 -0500

    agregado la excepcion de IOException

--- src/controladores/Main.java
+++ src/controladores/Main.java
@@ -1,10 +1,11 @@
 package controladores;
+import java.io.IOException;
 import java.sql.SQLException;
 
 import app.App;
 public class Main {
 
-	public static void main(String[] args) throws SQLException {
+	public static void main(String[] args) throws SQLException, IOException {
 		App app = new App();
 		app.start();
 	
