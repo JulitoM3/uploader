a3c585fe683f11733f2e0b7979fedfc5eefd5c44
Julio Guerrero <juliocaramelo44@gmail.com>
2022-08-11 17:23:55 -0500

    agregados los atributos a los modelos para crear el objeto

--- src/modelos/Compranet.java
+++ src/modelos/Compranet.java
@@ -2,4 +2,590 @@ package modelos;
 
 public class Compranet {
 
+	private String orden;
+    private String siglas_institucion;
+    private String institucion;
+    private String clave_unidad_compradora ;
+    private String nombre_unidad_compradora ;
+    private String respo_unidad_compradora ;
+    private String codigo_expediente ;
+    private String referencia_expediente ;
+    private String clave_cucop ;
+    private String titulo_expediente ;
+    private String plantilla_expediente ;
+    private String fundamento_legal ;
+    private String numero_procedimiento ;
+    private String fecha_fallo ;
+    private String fecha_publicacion ;
+    private String fecha_apertura ;
+    private String caracter_procedimiento ;
+    private String tipo_de_contratacion ;
+    private String tipo_procedimiento ;
+    private String medio_utilizado_propuestas ;
+    private String codigo_contrato ;
+    private String numero_control_contrato ;
+    private String titulo_contrato ;
+    private String descripcion_contrato ;
+    private String fecha_inicio_contrato ;
+    private String fecha_fin_contrato ;
+    private double importe_contrato;
+    private String moneda ;
+    private String estatus_contrato;
+    private boolean convenio_modificatorio ;
+    private String clave_programa_federal ;
+    private String fecha_firma_contrato ;
+    private String contrato_marco ;
+    private boolean compra_consolidada ;
+    private boolean contrato_plurianual ;
+    private String clave_cartera_shcp ;
+    private String folio_rupc ;
+    private String rfc ;
+    private String proveedor_contratista ;
+    private String estratificacion ;
+    private String clave_pais ;
+    private String rfc_verificado_sat ;
+    private String credito_externo ;
+    private String organismo_financiero ;
+    private String url_compranet ;
+	/**
+	 * @return the orden
+	 */
+	public String getOrden() {
+		return orden;
+	}
+	/**
+	 * @param orden the orden to set
+	 */
+	public void setOrden(String orden) {
+		this.orden = orden;
+	}
+	/**
+	 * @return the siglas_institucion
+	 */
+	public String getSiglas_institucion() {
+		return siglas_institucion;
+	}
+	/**
+	 * @param siglas_institucion the siglas_institucion to set
+	 */
+	public void setSiglas_institucion(String siglas_institucion) {
+		this.siglas_institucion = siglas_institucion;
+	}
+	/**
+	 * @return the institucion
+	 */
+	public String getInstitucion() {
+		return institucion;
+	}
+	/**
+	 * @param institucion the institucion to set
+	 */
+	public void setInstitucion(String institucion) {
+		this.institucion = institucion;
+	}
+	/**
+	 * @return the clave_unidad_compradora
+	 */
+	public String getClave_unidad_compradora() {
+		return clave_unidad_compradora;
+	}
+	/**
+	 * @param clave_unidad_compradora the clave_unidad_compradora to set
+	 */
+	public void setClave_unidad_compradora(String clave_unidad_compradora) {
+		this.clave_unidad_compradora = clave_unidad_compradora;
+	}
+	/**
+	 * @return the nombre_unidad_compradora
+	 */
+	public String getNombre_unidad_compradora() {
+		return nombre_unidad_compradora;
+	}
+	/**
+	 * @param nombre_unidad_compradora the nombre_unidad_compradora to set
+	 */
+	public void setNombre_unidad_compradora(String nombre_unidad_compradora) {
+		this.nombre_unidad_compradora = nombre_unidad_compradora;
+	}
+	/**
+	 * @return the respo_unidad_compradora
+	 */
+	public String getRespo_unidad_compradora() {
+		return respo_unidad_compradora;
+	}
+	/**
+	 * @param respo_unidad_compradora the respo_unidad_compradora to set
+	 */
+	public void setRespo_unidad_compradora(String respo_unidad_compradora) {
+		this.respo_unidad_compradora = respo_unidad_compradora;
+	}
+	/**
+	 * @return the codigo_expediente
+	 */
+	public String getCodigo_expediente() {
+		return codigo_expediente;
+	}
+	/**
+	 * @param codigo_expediente the codigo_expediente to set
+	 */
+	public void setCodigo_expediente(String codigo_expediente) {
+		this.codigo_expediente = codigo_expediente;
+	}
+	/**
+	 * @return the referencia_expediente
+	 */
+	public String getReferencia_expediente() {
+		return referencia_expediente;
+	}
+	/**
+	 * @param referencia_expediente the referencia_expediente to set
+	 */
+	public void setReferencia_expediente(String referencia_expediente) {
+		this.referencia_expediente = referencia_expediente;
+	}
+	/**
+	 * @return the clave_cucop
+	 */
+	public String getClave_cucop() {
+		return clave_cucop;
+	}
+	/**
+	 * @param clave_cucop the clave_cucop to set
+	 */
+	public void setClave_cucop(String clave_cucop) {
+		this.clave_cucop = clave_cucop;
+	}
+	/**
+	 * @return the titulo_expediente
+	 */
+	public String getTitulo_expediente() {
+		return titulo_expediente;
+	}
+	/**
+	 * @param titulo_expediente the titulo_expediente to set
+	 */
+	public void setTitulo_expediente(String titulo_expediente) {
+		this.titulo_expediente = titulo_expediente;
+	}
+	/**
+	 * @return the plantilla_expediente
+	 */
+	public String getPlantilla_expediente() {
+		return plantilla_expediente;
+	}
+	/**
+	 * @param plantilla_expediente the plantilla_expediente to set
+	 */
+	public void setPlantilla_expediente(String plantilla_expediente) {
+		this.plantilla_expediente = plantilla_expediente;
+	}
+	/**
+	 * @return the fundamento_legal
+	 */
+	public String getFundamento_legal() {
+		return fundamento_legal;
+	}
+	/**
+	 * @param fundamento_legal the fundamento_legal to set
+	 */
+	public void setFundamento_legal(String fundamento_legal) {
+		this.fundamento_legal = fundamento_legal;
+	}
+	/**
+	 * @return the numero_procedimiento
+	 */
+	public String getNumero_procedimiento() {
+		return numero_procedimiento;
+	}
+	/**
+	 * @param numero_procedimiento the numero_procedimiento to set
+	 */
+	public void setNumero_procedimiento(String numero_procedimiento) {
+		this.numero_procedimiento = numero_procedimiento;
+	}
+	/**
+	 * @return the fecha_publicacion
+	 */
+	public String getFecha_publicacion() {
+		return fecha_publicacion;
+	}
+	/**
+	 * @param fecha_publicacion the fecha_publicacion to set
+	 */
+	public void setFecha_publicacion(String fecha_publicacion) {
+		this.fecha_publicacion = fecha_publicacion;
+	}
+	/**
+	 * @return the fecha_fallo
+	 */
+	public String getFecha_fallo() {
+		return fecha_fallo;
+	}
+	/**
+	 * @param fecha_fallo the fecha_fallo to set
+	 */
+	public void setFecha_fallo(String fecha_fallo) {
+		this.fecha_fallo = fecha_fallo;
+	}
+	/**
+	 * @return the fecha_apertura
+	 */
+	public String getFecha_apertura() {
+		return fecha_apertura;
+	}
+	/**
+	 * @param fecha_apertura the fecha_apertura to set
+	 */
+	public void setFecha_apertura(String fecha_apertura) {
+		this.fecha_apertura = fecha_apertura;
+	}
+	/**
+	 * @return the caracter_procedimiento
+	 */
+	public String getCaracter_procedimiento() {
+		return caracter_procedimiento;
+	}
+	/**
+	 * @param caracter_procedimiento the caracter_procedimiento to set
+	 */
+	public void setCaracter_procedimiento(String caracter_procedimiento) {
+		this.caracter_procedimiento = caracter_procedimiento;
+	}
+	/**
+	 * @return the tipo_de_contratacion
+	 */
+	public String getTipo_de_contratacion() {
+		return tipo_de_contratacion;
+	}
+	/**
+	 * @param tipo_de_contratacion the tipo_de_contratacion to set
+	 */
+	public void setTipo_de_contratacion(String tipo_de_contratacion) {
+		this.tipo_de_contratacion = tipo_de_contratacion;
+	}
+	/**
+	 * @return the tipo_procedimiento
+	 */
+	public String getTipo_procedimiento() {
+		return tipo_procedimiento;
+	}
+	/**
+	 * @param tipo_procedimiento the tipo_procedimiento to set
+	 */
+	public void setTipo_procedimiento(String tipo_procedimiento) {
+		this.tipo_procedimiento = tipo_procedimiento;
+	}
+	/**
+	 * @return the medio_utilizado_propuestas
+	 */
+	public String getMedio_utilizado_propuestas() {
+		return medio_utilizado_propuestas;
+	}
+	/**
+	 * @param medio_utilizado_propuestas the medio_utilizado_propuestas to set
+	 */
+	public void setMedio_utilizado_propuestas(String medio_utilizado_propuestas) {
+		this.medio_utilizado_propuestas = medio_utilizado_propuestas;
+	}
+	/**
+	 * @return the codigo_contrato
+	 */
+	public String getCodigo_contrato() {
+		return codigo_contrato;
+	}
+	/**
+	 * @param codigo_contrato the codigo_contrato to set
+	 */
+	public void setCodigo_contrato(String codigo_contrato) {
+		this.codigo_contrato = codigo_contrato;
+	}
+	/**
+	 * @return the numero_control_contrato
+	 */
+	public String getNumero_control_contrato() {
+		return numero_control_contrato;
+	}
+	/**
+	 * @param numero_control_contrato the numero_control_contrato to set
+	 */
+	public void setNumero_control_contrato(String numero_control_contrato) {
+		this.numero_control_contrato = numero_control_contrato;
+	}
+	/**
+	 * @return the titulo_contrato
+	 */
+	public String getTitulo_contrato() {
+		return titulo_contrato;
+	}
+	/**
+	 * @param titulo_contrato the titulo_contrato to set
+	 */
+	public void setTitulo_contrato(String titulo_contrato) {
+		this.titulo_contrato = titulo_contrato;
+	}
+	/**
+	 * @return the descripcion_contrato
+	 */
+	public String getDescripcion_contrato() {
+		return descripcion_contrato;
+	}
+	/**
+	 * @param descripcion_contrato the descripcion_contrato to set
+	 */
+	public void setDescripcion_contrato(String descripcion_contrato) {
+		this.descripcion_contrato = descripcion_contrato;
+	}
+	/**
+	 * @return the fecha_inicio_contrato
+	 */
+	public String getFecha_inicio_contrato() {
+		return fecha_inicio_contrato;
+	}
+	/**
+	 * @param fecha_inicio_contrato the fecha_inicio_contrato to set
+	 */
+	public void setFecha_inicio_contrato(String fecha_inicio_contrato) {
+		this.fecha_inicio_contrato = fecha_inicio_contrato;
+	}
+	/**
+	 * @return the fecha_fin_contrato
+	 */
+	public String getFecha_fin_contrato() {
+		return fecha_fin_contrato;
+	}
+	/**
+	 * @param fecha_fin_contrato the fecha_fin_contrato to set
+	 */
+	public void setFecha_fin_contrato(String fecha_fin_contrato) {
+		this.fecha_fin_contrato = fecha_fin_contrato;
+	}
+	/**
+	 * @return the importe_contrato
+	 */
+	public double getImporte_contrato() {
+		return importe_contrato;
+	}
+	/**
+	 * @param importe_contrato the importe_contrato to set
+	 */
+	public void setImporte_contrato(double importe_contrato) {
+		this.importe_contrato = importe_contrato;
+	}
+	/**
+	 * @return the moneda
+	 */
+	public String getMoneda() {
+		return moneda;
+	}
+	/**
+	 * @param moneda the moneda to set
+	 */
+	public void setMoneda(String moneda) {
+		this.moneda = moneda;
+	}
+	/**
+	 * @return the estatus_contrato
+	 */
+	public String getEstatus_contrato() {
+		return estatus_contrato;
+	}
+	/**
+	 * @param estatus_contrato the estatus_contrato to set
+	 */
+	public void setEstatus_contrato(String estatus_contrato) {
+		this.estatus_contrato = estatus_contrato;
+	}
+	/**
+	 * @return the convenio_modificatorio
+	 */
+	public boolean isConvenio_modificatorio() {
+		return convenio_modificatorio;
+	}
+	/**
+	 * @param convenio_modificatorio the convenio_modificatorio to set
+	 */
+	public void setConvenio_modificatorio(boolean convenio_modificatorio) {
+		this.convenio_modificatorio = convenio_modificatorio;
+	}
+	/**
+	 * @return the clave_programa_federal
+	 */
+	public String getClave_programa_federal() {
+		return clave_programa_federal;
+	}
+	/**
+	 * @param clave_programa_federal the clave_programa_federal to set
+	 */
+	public void setClave_programa_federal(String clave_programa_federal) {
+		this.clave_programa_federal = clave_programa_federal;
+	}
+	/**
+	 * @return the fecha_firma_contrato
+	 */
+	public String getFecha_firma_contrato() {
+		return fecha_firma_contrato;
+	}
+	/**
+	 * @param fecha_firma_contrato the fecha_firma_contrato to set
+	 */
+	public void setFecha_firma_contrato(String fecha_firma_contrato) {
+		this.fecha_firma_contrato = fecha_firma_contrato;
+	}
+	/**
+	 * @return the contrato_marco
+	 */
+	public String getContrato_marco() {
+		return contrato_marco;
+	}
+	/**
+	 * @param contrato_marco the contrato_marco to set
+	 */
+	public void setContrato_marco(String contrato_marco) {
+		this.contrato_marco = contrato_marco;
+	}
+	/**
+	 * @return the compra_consolidada
+	 */
+	public boolean isCompra_consolidada() {
+		return compra_consolidada;
+	}
+	/**
+	 * @param compra_consolidada the compra_consolidada to set
+	 */
+	public void setCompra_consolidada(boolean compra_consolidada) {
+		this.compra_consolidada = compra_consolidada;
+	}
+	/**
+	 * @return the contrato_plurianual
+	 */
+	public boolean isContrato_plurianual() {
+		return contrato_plurianual;
+	}
+	/**
+	 * @param contrato_plurianual the contrato_plurianual to set
+	 */
+	public void setContrato_plurianual(boolean contrato_plurianual) {
+		this.contrato_plurianual = contrato_plurianual;
+	}
+	/**
+	 * @return the clave_cartera_shcp
+	 */
+	public String getClave_cartera_shcp() {
+		return clave_cartera_shcp;
+	}
+	/**
+	 * @param clave_cartera_shcp the clave_cartera_shcp to set
+	 */
+	public void setClave_cartera_shcp(String clave_cartera_shcp) {
+		this.clave_cartera_shcp = clave_cartera_shcp;
+	}
+	/**
+	 * @return the folio_rupc
+	 */
+	public String getFolio_rupc() {
+		return folio_rupc;
+	}
+	/**
+	 * @param folio_rupc the folio_rupc to set
+	 */
+	public void setFolio_rupc(String folio_rupc) {
+		this.folio_rupc = folio_rupc;
+	}
+	/**
+	 * @return the rfc
+	 */
+	public String getRfc() {
+		return rfc;
+	}
+	/**
+	 * @param rfc the rfc to set
+	 */
+	public void setRfc(String rfc) {
+		this.rfc = rfc;
+	}
+	/**
+	 * @return the proveedor_contratista
+	 */
+	public String getProveedor_contratista() {
+		return proveedor_contratista;
+	}
+	/**
+	 * @param proveedor_contratista the proveedor_contratista to set
+	 */
+	public void setProveedor_contratista(String proveedor_contratista) {
+		this.proveedor_contratista = proveedor_contratista;
+	}
+	/**
+	 * @return the estratificacion
+	 */
+	public String getEstratificacion() {
+		return estratificacion;
+	}
+	/**
+	 * @param estratificacion the estratificacion to set
+	 */
+	public void setEstratificacion(String estratificacion) {
+		this.estratificacion = estratificacion;
+	}
+	/**
+	 * @return the clave_pais
+	 */
+	public String getClave_pais() {
+		return clave_pais;
+	}
+	/**
+	 * @param clave_pais the clave_pais to set
+	 */
+	public void setClave_pais(String clave_pais) {
+		this.clave_pais = clave_pais;
+	}
+	/**
+	 * @return the rfc_verificado_sat
+	 */
+	public String getRfc_verificado_sat() {
+		return rfc_verificado_sat;
+	}
+	/**
+	 * @param rfc_verificado_sat the rfc_verificado_sat to set
+	 */
+	public void setRfc_verificado_sat(String rfc_verificado_sat) {
+		this.rfc_verificado_sat = rfc_verificado_sat;
+	}
+	/**
+	 * @return the credito_externo
+	 */
+	public String getCredito_externo() {
+		return credito_externo;
+	}
+	/**
+	 * @param credito_externo the credito_externo to set
+	 */
+	public void setCredito_externo(String credito_externo) {
+		this.credito_externo = credito_externo;
+	}
+	/**
+	 * @return the organismo_financiero
+	 */
+	public String getOrganismo_financiero() {
+		return organismo_financiero;
+	}
+	/**
+	 * @param organismo_financiero the organismo_financiero to set
+	 */
+	public void setOrganismo_financiero(String organismo_financiero) {
+		this.organismo_financiero = organismo_financiero;
+	}
+	/**
+	 * @return the url_compranet
+	 */
+	public String getUrl_compranet() {
+		return url_compranet;
+	}
+	/**
+	 * @param url_compranet the url_compranet to set
+	 */
+	public void setUrl_compranet(String url_compranet) {
+		this.url_compranet = url_compranet;
+	}
+
 }
